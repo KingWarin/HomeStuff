b147cab08b5b0ed9ba70588c11fa3158f19102a3
KingWarin <info@kingwarin.de>
2015-11-12 20:35:24 +0100

    Added a sample php service for data-retrieval.

---
+++ service.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    require("dbconnect.php");
+
+    if(isset($_SESSION['user']) && isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+        mysqli_query($con, 'SET CHARACTER SET utf8');
+
+        if(isset($_GET['type']) && $_GET['type'] == 'heater') {
+            $query = "SELECT * FROM heater";
+            $result = mysqli_query($con, $query) or die('Cannot load data from db');
+            $heater_data = array();
+            if(mysqli_num_rows($result)) {
+                while ($heat_day = mysqli_fetch_assoc($result)) {
+                    $heater_data[] = $heat_day;
+                }
+            }
+
+            header('Content-type: application/json');
+            echo json_encode(array('heating'=>$heater_data));
+
+            @mysqli_close($con);
+        }
+        elseif(isset($_POST['type']) && $_POST['type'] == 'new_heat_date' && isset($_POST['heat_data'])) {
+            //do stuff with the heat data... which means decode the json and push it
+            // to the db
+        }
+
+    }
+?>
