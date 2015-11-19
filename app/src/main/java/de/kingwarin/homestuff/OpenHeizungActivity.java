a5a8bb89463e9c76758a54009a046bb488eb75e5
Sven Hoth <svh@solute.de>
2015-11-19 08:40:26 +0100

    Moved the asynchrounus data-download to the open-view.

--- app/src/main/java/de/kingwarin/homestuff/OpenHeizungActivity.java
+++ app/src/main/java/de/kingwarin/homestuff/OpenHeizungActivity.java
@@ -1,11 +1,27 @@
 package de.kingwarin.homestuff;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class OpenHeizungActivity extends ActionBarActivity {
+    private String heater_data;
+    private Intent intent;
+    public final static String heater_value = "None";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +31,86 @@ public class OpenHeizungActivity extends ActionBarActivity {
     }
 
     public void addHeizung(View view) {
-        Intent intent = new Intent(this, AddHeizungActivity.class);
+        intent = new Intent(this, AddHeizungActivity.class);
         startActivity(intent);
     }
 
     public void showHeizung(View view) {
-        Intent intent = new Intent(this, ShowHeizungActivity.class);
-        startActivity(intent);
+        intent = new Intent(this, ShowHeizungActivity.class);
+        fetchHeaterData();
+    }
+
+    public void fetchHeaterData() {
+        String stringUrl = "http://www.kingwarin.de/homestuff/service.php?type=heater";
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            heater_data = "No network connection available.";
+            intent.putExtra(heater_value, heater_data);
+            startActivity(intent);
+        }
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            heater_data = result;
+            intent.putExtra(heater_value, heater_data);
+            startActivity(intent);
+        }
+
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                // Log.d(DEBUG_TAG, "The response is: " + response);
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+
+        public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
     }
 }
